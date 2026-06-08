16184697f4de777d272f6cfb881be918511a696b
Z User <z@container>
2026-06-08 20:17:32 +0000

    Change separator lines to gray (&7)

--- src/main/java/com/fabian/xsetwarps/XSetWarps.java
+++ src/main/java/com/fabian/xsetwarps/XSetWarps.java
@@ -95,7 +95,7 @@ public class XSetWarps extends JavaPlugin {
         }
 
         getServer().getConsoleSender().sendMessage(
-                ColorUtils.translateColors("&8[&bX-SetWarps&8] &3----------------------------------------------"));
+                ColorUtils.translateColors("&8[&bX-SetWarps&8] &7----------------------------------------------"));
         getServer().getConsoleSender().sendMessage(
                 ColorUtils.translateColors("&8[&bX-SetWarps&8]   &aEnabled v" + version + "! Enjoy warping!"));
         getServer().getConsoleSender().sendMessage(
@@ -103,7 +103,7 @@ public class XSetWarps extends JavaPlugin {
         getServer().getConsoleSender()
                 .sendMessage(ColorUtils.translateColors("&8[&bX-SetWarps&8]   &fPlaceholderAPI: " + papiStatus));
         getServer().getConsoleSender().sendMessage(
-                ColorUtils.translateColors("&8[&bX-SetWarps&8] &3----------------------------------------------"));
+                ColorUtils.translateColors("&8[&bX-SetWarps&8] &7----------------------------------------------"));
 
         if (getConfig().getBoolean("updates.check", true)) {
             updateChecker.checkForUpdates();
@@ -129,11 +129,11 @@ public class XSetWarps extends JavaPlugin {
         String version = getDescription().getVersion();
 
         getServer().getConsoleSender().sendMessage(
-                ColorUtils.translateColors("&8[&bX-SetWarps&8] &3----------------------------------------------"));
+                ColorUtils.translateColors("&8[&bX-SetWarps&8] &7----------------------------------------------"));
         getServer().getConsoleSender().sendMessage(
                 ColorUtils.translateColors("&8[&bX-SetWarps&8]   &cDisabled v" + version + "! Goodbye."));
         getServer().getConsoleSender().sendMessage(
-                ColorUtils.translateColors("&8[&bX-SetWarps&8] &3----------------------------------------------"));
+                ColorUtils.translateColors("&8[&bX-SetWarps&8] &7----------------------------------------------"));
     }
 
     public WarpManager getWarpManager() {
